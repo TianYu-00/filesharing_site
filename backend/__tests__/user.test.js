d218f47902ae778369fdba7d3a00562ba38cea04
TianYu-00 <imniyeye@gmail.com>
2024-11-11 13:08:34 +0000

    update user tests

--- backend/__tests__/user.test.js
+++ backend/__tests__/user.test.js
@@ -35,18 +35,6 @@ describe("Users Tests", () => {
     expect(result.body.success).toBe(true);
     expect(result.body.data).toHaveProperty("username", newUser.username);
   });
-});
-
-describe("User Login Tests", () => {
-  beforeEach(async () => {
-    const newUser = {
-      username: "testuser",
-      email: "testuser@example.com",
-      password: "testpassword123",
-    };
-
-    await request(app).post("/api/users/register").send(newUser);
-  });
 
   test("/api/users/login should successfully log in a user", async () => {
     const loginUser = {
@@ -54,35 +42,19 @@ describe("User Login Tests", () => {
       password: "testpassword123",
     };
 
+    const newUser = {
+      username: "testuser",
+      email: "testuser@example.com",
+      password: "testpassword123",
+    };
+
+    await request(app).post("/api/users/register").send(newUser);
+
     const result = await request(app).post("/api/users/login").send(loginUser);
     expect(result.status).toBe(200);
     expect(result.body.success).toBe(true);
     expect(result.body.msg).toBe("Login approved");
     expect(result.body.data).toHaveProperty("username");
     expect(result.body.data.username).toBe("testuser");
-  });
-
-  test("/api/users/login should return an error for incorrect email", async () => {
-    const loginUser = {
-      email: "nonexistentuser@example.com",
-      password: "somepassword123",
-    };
-
-    const result = await request(app).post("/api/users/login").send(loginUser);
-    expect(result.status).toBe(400);
-    expect(result.body.success).toBe(false);
-    expect(result.body.msg).toBe("User not found");
-  });
-
-  test("/api/users/login should return an error for incorrect password", async () => {
-    const loginUser = {
-      email: "testuser@example.com",
-      password: "wrongpassword123",
-    };
-
-    const result = await request(app).post("/api/users/login").send(loginUser);
-    expect(result.status).toBe(400);
-    expect(result.body.success).toBe(false);
-    expect(result.body.msg).toBe("Incorrect password");
   });
 });
