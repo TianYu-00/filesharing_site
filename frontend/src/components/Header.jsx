353de9046d6976d50fac9e2a8d7b2ac74958a45e
TianYu-00 <imniyeye@gmail.com>
2024-11-23 00:31:38 +0000

    update header to use react spring instead

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo_simple.png";
 import { Link } from "react-router-dom";
 import { BsList } from "react-icons/bs";
 import { useUser } from "../context/UserContext";
+import { useSpring, animated } from "@react-spring/web";
 
 function Header() {
   const [isMenuClicked, setIsMenuClicked] = useState(false);
@@ -12,43 +13,32 @@ function Header() {
     userVerify();
   }, []);
 
-  useEffect(() => {
-    if (user) {
-      // console.log(user);
-    }
-  }, [user]);
-
   const handle_MenuClick = () => {
     setIsMenuClicked(!isMenuClicked);
   };
 
+  const menuAnimation = useSpring({
+    height: isMenuClicked ? "94vh" : "0",
+    opacity: isMenuClicked ? 1 : 0,
+    overflow: "hidden",
+  });
+
   return (
-    <div
-      className={` w-screen absolute transition-all duration-700 ease-in-out ${
-        isMenuClicked ? "max-h-screen" : "max-h-20"
-      } bg-[#111313] z-50`}
-    >
+    <div className="w-screen absolute bg-[#111313] z-50">
       <div className="flex items-center p-4">
         <div className="flex-shrink-0 hidden md:block">
           <img src={logo} className="h-10" alt="Logo" />
         </div>
-        <div className="flex-grow flex items-center justify-end text-white ">
-          {user ? (
-            <div className="mr-5"> Logged in as {user.username}</div>
-          ) : (
-            <div className="mr-5"> Not logged in</div>
-          )}
+        <div className="flex-grow flex items-center justify-end text-white">
+          {user ? <div className="mr-5">Logged in as {user.username}</div> : <div className="mr-5">Not logged in</div>}
           <button onClick={handle_MenuClick} className="hover:text-blue-500">
             <BsList size={30} />
           </button>
         </div>
       </div>
 
-      <div
-        className={`text-white w-full transition-all duration-700 ease-in-out ${
-          isMenuClicked ? "h-[calc(100vh-4.7rem)]" : "h-0"
-        } overflow-hidden`}
-      >
+      {/* Menu */}
+      <animated.div style={menuAnimation} className="text-white w-full">
         <ul className="space-y-1 flex flex-col items-center pt-5">
           {!user && (
             <li className="w-full">
@@ -112,7 +102,7 @@ function Header() {
             </Link>
           </li>
         </ul>
-      </div>
+      </animated.div>
     </div>
   );
 }
