4c1f1b7b8579326133a58f287a3370e0403cc8c6
TianYu-00 <imniyeye@gmail.com>
2024-11-23 01:06:59 +0000

    add more visual feedbacks to file uploading

--- frontend/src/pages/Landing_Home.jsx
+++ frontend/src/pages/Landing_Home.jsx
@@ -24,7 +24,7 @@ function Home() {
   //
   const [downloadButtonToolTipContent, setDownloadButtonToolTipContent] = useState("Redirect to download page");
   const [linkButtonToolTipContent, setLinkButtonToolTipContent] = useState("Copy download link to clipboard");
-  const [reselectButtonToolTipContent, setReselectButtonToolTipContent] = useState("Reselect a file to upload");
+  const [reselectButtonToolTipContent, setReselectButtonToolTipContent] = useState("Upload another file");
 
   //
   const { user } = useUser();
@@ -99,25 +99,27 @@ function Home() {
       {isUploadClicked &&
         (uploadProgress === 100 ? (
           <div className="w-full mx-auto flex mt-2 justify-center">
-            <p>File has been uploaded</p>
+            <p className="text-green-500 font-bold">File has been uploaded</p>
           </div>
         ) : (
           <div className="w-full mx-auto flex mt-2 justify-center">
-            <p>File is being uploaded...</p>
+            <p className="text-orange-500 font-bold">File is being uploaded...</p>
           </div>
         ))}
 
       {selectedFile && (
         <div className="grid grid-cols-3 w-full mx-auto mt-6">
           {/* Header Row */}
-          <div className="border-b border-gray-700 p-2 text-left">Name</div>
-          <div className="border-b border-gray-700 p-2 text-left">Size</div>
-          <div className="border-b border-gray-700 p-2 text-left">Type</div>
+          <div className="border-b border-gray-700 p-2 text-left font-bold">Name</div>
+          <div className="border-b border-gray-700 p-2 text-left font-bold">Size</div>
+          <div className="border-b border-gray-700 p-2 text-left font-bold">Type</div>
 
           {/* Data Row */}
-          <div className="border-gray-700 p-2 text-left">{selectedFile.name}</div>
-          <div className="border-gray-700 p-2 text-left">{fileSizeFormatter(selectedFile.size)}</div>
-          <div className="border-gray-700 p-2 text-left">{selectedFile.type}</div>
+          <div className="border-gray-700 p-2 text-left overflow-hidden truncate">{selectedFile.name}</div>
+          <div className="border-gray-700 p-2 text-left overflow-hidden truncate">
+            {fileSizeFormatter(selectedFile.size)}
+          </div>
+          <div className="border-gray-700 p-2 text-left overflow-hidden truncate">{selectedFile.type}</div>
         </div>
       )}
 
@@ -125,7 +127,7 @@ function Home() {
         <div className="flex mt-5">
           <button
             onClick={handle_FileUpload}
-            className="bg-blue-500 p-1 rounded flex items-center justify-center w-full font-bold"
+            className="bg-blue-500 p-2 rounded flex items-center justify-center w-full font-bold"
           >
             <BsUpload className="mx-2 stroke-1" size={20} /> Upload
           </button>
