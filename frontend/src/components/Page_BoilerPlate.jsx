11563a8d48c670b72743c29065ba3f901b77044f
TianYu-00 <imniyeye@gmail.com>
2024-11-25 19:11:29 +0000

    add some styling techniques to make mobile view better

--- frontend/src/components/Page_BoilerPlate.jsx
+++ frontend/src/components/Page_BoilerPlate.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/logo.png";
 
 function Page_BoilerPlate({ children }) {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="max-w-[1280px] mx-auto w-full flex items-center justify-center">
-        <div className="max-w-[1000px] w-full p-8 text-center text-white rounded-2xl m-4 bg-neutral-900 shadow-lg">
-          <div className="flex justify-center mb-14 bg-blue-600 rounded-t-2xl">
+        <div className="max-w-[1000px] w-full p-8 text-center text-white rounded-2xl m-4 md:bg-neutral-900 md:shadow-lg">
+          <div className="hidden md:flex justify-center mb-14 bg-blue-600 rounded-t-2xl">
             <img src={logo} alt="" className="p-6 max-w-full md:max-w-[500px]" />
           </div>
           {children}
