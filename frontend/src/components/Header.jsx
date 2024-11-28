205b56254e67d97ee005a56f449eeb4b03893e52
TianYu-00 <imniyeye@gmail.com>
2024-11-28 23:25:22 +0000

    adjust spacing in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -25,85 +25,91 @@ function Header() {
   });
 
   return (
-    <div className="w-full absolute bg-[#111313] z-50">
-      <div className="flex items-center p-4">
-        <div className="flex-shrink-0 hidden md:block cursor-pointer" onClick={() => navigate("/")}>
-          <img src={logo} className="h-10" alt="Logo" />
+    <div className="max-w-full h-20 bg-[#111313] relative">
+      <div className="w-full absolute bg-[#111313] z-50 ">
+        <div className="flex items-center p-4">
+          <div className="flex-shrink-0 hidden md:block cursor-pointer" onClick={() => navigate("/")}>
+            <img src={logo} className="h-12" alt="Logo" />
+          </div>
+          <div className="flex-grow flex items-center justify-end text-white">
+            {user ? (
+              <div className="mr-5">Logged in as {user.username}</div>
+            ) : (
+              <div className="mr-5">Not logged in</div>
+            )}
+            <button onClick={handle_MenuClick} className="hover:text-blue-500">
+              <BsList size={30} />
+            </button>
+          </div>
         </div>
-        <div className="flex-grow flex items-center justify-end text-white">
-          {user ? <div className="mr-5">Logged in as {user.username}</div> : <div className="mr-5">Not logged in</div>}
-          <button onClick={handle_MenuClick} className="hover:text-blue-500">
-            <BsList size={30} />
-          </button>
-        </div>
+
+        {/* Menu */}
+        <animated.div style={menuAnimation} className="text-white w-full">
+          <ul className="space-y-1 flex flex-col items-center pt-5">
+            {!user && (
+              <li className="w-full">
+                <Link
+                  to="/auth"
+                  className="hover:bg-neutral-700 w-full flex justify-center p-2"
+                  onClick={handle_MenuClick}
+                >
+                  Login / Register
+                </Link>
+              </li>
+            )}
+
+            {/* My Files */}
+            {user && (
+              <li className="w-full">
+                <Link
+                  to="/my-files"
+                  className="hover:bg-neutral-700 w-full flex justify-center p-2"
+                  onClick={handle_MenuClick}
+                >
+                  My Files
+                </Link>
+              </li>
+            )}
+
+            {/* Account Settings */}
+            {user && (
+              <li className="w-full">
+                <Link
+                  to="/account"
+                  className="hover:bg-neutral-700 w-full flex justify-center p-2"
+                  onClick={handle_MenuClick}
+                >
+                  Account Settings
+                </Link>
+              </li>
+            )}
+
+            {user && (
+              <li className="w-full">
+                <Link
+                  to="/home"
+                  className="hover:bg-neutral-700 w-full flex justify-center p-2"
+                  onClick={() => {
+                    handle_MenuClick();
+                    userLogout();
+                  }}
+                >
+                  Log out
+                </Link>
+              </li>
+            )}
+
+            <li className="w-full border border-neutral-800 m-5"></li>
+
+            {/* Home */}
+            <li className="w-full">
+              <Link to="/" className="hover:bg-neutral-700 w-full flex justify-center p-2" onClick={handle_MenuClick}>
+                Home
+              </Link>
+            </li>
+          </ul>
+        </animated.div>
       </div>
-
-      {/* Menu */}
-      <animated.div style={menuAnimation} className="text-white w-full">
-        <ul className="space-y-1 flex flex-col items-center pt-5">
-          {!user && (
-            <li className="w-full">
-              <Link
-                to="/auth"
-                className="hover:bg-neutral-700 w-full flex justify-center p-2"
-                onClick={handle_MenuClick}
-              >
-                Login / Register
-              </Link>
-            </li>
-          )}
-
-          {/* My Files */}
-          {user && (
-            <li className="w-full">
-              <Link
-                to="/my-files"
-                className="hover:bg-neutral-700 w-full flex justify-center p-2"
-                onClick={handle_MenuClick}
-              >
-                My Files
-              </Link>
-            </li>
-          )}
-
-          {/* Account Settings */}
-          {user && (
-            <li className="w-full">
-              <Link
-                to="/account"
-                className="hover:bg-neutral-700 w-full flex justify-center p-2"
-                onClick={handle_MenuClick}
-              >
-                Account Settings
-              </Link>
-            </li>
-          )}
-
-          {user && (
-            <li className="w-full">
-              <Link
-                to="/home"
-                className="hover:bg-neutral-700 w-full flex justify-center p-2"
-                onClick={() => {
-                  handle_MenuClick();
-                  userLogout();
-                }}
-              >
-                Log out
-              </Link>
-            </li>
-          )}
-
-          <li className="w-full border border-neutral-800 m-5"></li>
-
-          {/* Home */}
-          <li className="w-full">
-            <Link to="/" className="hover:bg-neutral-700 w-full flex justify-center p-2" onClick={handle_MenuClick}>
-              Home
-            </Link>
-          </li>
-        </ul>
-      </animated.div>
     </div>
   );
 }
