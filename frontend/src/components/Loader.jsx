3f6ee74db1cd96876e4bab8578bbaa36f16bcc98
TianYu-00 <imniyeye@gmail.com>
2024-12-05 10:29:46 +0000

    add size property to loader for custom size

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Loader = () => (
+const Loader = ({ size = 4 }) => (
   <svg
     aria-hidden="true"
     role="status"
-    className="inline w-4 h-4 me-3 text-white animate-spin"
+    className={`inline w-${size} h-${size} me-3 text-white animate-spin`}
     viewBox="0 0 100 101"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
