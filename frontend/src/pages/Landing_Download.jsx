28cffa8b236e0939c396e1acc8124416e8d2b537
TianYu-00 <imniyeye@gmail.com>
2024-12-09 18:44:53 +0000

    home page style dark theme

--- frontend/src/pages/Landing_Download.jsx
+++ frontend/src/pages/Landing_Download.jsx
@@ -198,37 +198,37 @@ function Landing_Download() {
 
       {file && (
         <div>
-          <div className="grid grid-cols-2 w-full mx-auto mt-6">
-            <div className="border border-gray-700 p-2 text-left">File ID</div>
-            <div className="border border-gray-700 p-2 text-left">{file.id}</div>
+          <div className="grid grid-cols-2 w-full mx-auto mt-6 text-copy-primary">
+            <div className="border border-border p-2 text-left">File ID</div>
+            <div className="border border-border p-2 text-left">{file.id}</div>
 
-            <div className="border border-gray-700 p-2 text-left">File Name</div>
-            <div className="border border-gray-700 p-2 text-left whitespace-nowrap overflow-hidden truncate">
+            <div className="border border-border p-2 text-left">File Name</div>
+            <div className="border border-border p-2 text-left whitespace-nowrap overflow-hidden truncate">
               {file.originalname}
             </div>
 
-            <div className="border border-gray-700 p-2 text-left">File Size</div>
-            <div className="border border-gray-700 p-2 text-left">{fileSizeFormatter(file.size)}</div>
+            <div className="border border-border p-2 text-left">File Size</div>
+            <div className="border border-border p-2 text-left">{fileSizeFormatter(file.size)}</div>
 
-            <div className="border border-gray-700 p-2 text-left">File Type</div>
-            <div className="border border-gray-700 p-2 text-left">{file.mimetype}</div>
+            <div className="border border-border p-2 text-left">File Type</div>
+            <div className="border border-border p-2 text-left">{file.mimetype}</div>
 
-            <div className="border border-gray-700 p-2 text-left">File Created Time</div>
-            <div className="border border-gray-700 p-2 text-left">{fileDateFormatter(file.created_at)[0]}</div>
+            <div className="border border-border p-2 text-left">File Created Time</div>
+            <div className="border border-border p-2 text-left">{fileDateFormatter(file.created_at)[0]}</div>
 
-            <div className="border border-gray-700 p-2 text-left">File Created Date</div>
-            <div className="border border-gray-700 p-2 text-left">{fileDateFormatter(file.created_at)[1]}</div>
+            <div className="border border-border p-2 text-left">File Created Date</div>
+            <div className="border border-border p-2 text-left">{fileDateFormatter(file.created_at)[1]}</div>
 
-            <div className="border border-gray-700 p-2 text-left">File Owner ID</div>
-            <div className="border border-gray-700 p-2 text-left">{file.user_id}</div>
+            <div className="border border-border p-2 text-left">File Owner ID</div>
+            <div className="border border-border p-2 text-left">{file.user_id}</div>
 
-            <div className="border border-gray-700 p-2 text-left">Downloads</div>
-            <div className="border border-gray-700 p-2 text-left">{downloadLinkInfo?.download_count || "N/A"}</div>
+            <div className="border border-border p-2 text-left">Downloads</div>
+            <div className="border border-border p-2 text-left">{downloadLinkInfo?.download_count || "N/A"}</div>
           </div>
 
           <div>
             <button
-              className="mt-4"
+              className="mt-4 text-copy-primary hover:text-copy-opp hover:bg-background-opp p-1 rounded-md"
               onClick={copyLinkToClipboard}
               data-tooltip-id="id_link_button"
               data-tooltip-content={tooltipContent}
@@ -245,7 +245,7 @@ function Landing_Download() {
 
           <button
             onClick={handle_DownloadFile}
-            className="bg-blue-500 p-2 rounded max-w-[200px] w-full mt-4 transition duration-500 ease-in-out hover:bg-blue-700 font-bold"
+            className="p-2 rounded max-w-[200px] w-full mt-4 transition duration-500 ease-in-out font-bold bg-cta hover:bg-cta-active text-cta-text"
           >
             Download
           </button>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     extend: {
       colors: {
         background: "rgba(var(--background))",
+        "background-opp": "rgba(var(--background-opp))",
         border: "rgba(var(--border))",
         card: "rgba(var(--card))",
         "copy-primary": "rgba(var(--copy-primary))",
         "copy-secondary": "rgba(var(--copy-secondary))",
+        "copy-opp": "rgba(var(--copy-opp))",
         cta: "rgba(var(--cta))",
         "cta-active": "rgba(var(--cta-active))",
         "cta-text": "rgba(var(--cta-text))",
