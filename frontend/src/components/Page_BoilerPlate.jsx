9c54b77b3ac4de67851f307cfdc3ab516a41c832
TianYu-00 <imniyeye@gmail.com>
2024-12-17 18:22:20 +0000

    update boiler plate logo

--- frontend/src/components/Page_BoilerPlate.jsx
+++ frontend/src/components/Page_BoilerPlate.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logoNoBackground from "../assets/logo-no-background-white.svg";
 
 function Page_BoilerPlate({ children }) {
   return (
@@ -8,7 +8,7 @@ function Page_BoilerPlate({ children }) {
         <div className="max-w-[1000px] w-full p-8 text-center text-primary rounded-2xl m-4 md:bg-card md:shadow-lg">
           {/* image */}
           <div className="hidden md:flex justify-center mb-14 bg-cta rounded-t-2xl">
-            <img src={logo} alt="" className="p-6 max-w-full md:max-w-[500px]" />
+            <img src={logoNoBackground} alt="" className="p-6 max-w-full md:max-w-[800px]" />
             {/* <div className="p-6 max-w-full w-full md:max-w-[500px]">
               <h1 className="text-white/90 font-black text-7xl ">Dropboxer</h1>
               <h2 className="text-white/90 font-bold text-2xl mt-3">Select - Upload - Share</h2>
