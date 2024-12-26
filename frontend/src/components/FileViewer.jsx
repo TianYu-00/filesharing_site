885d186a26786251242b7c9cf05e2c018c3b77d1
TianYu-00 <imniyeye@gmail.com>
2024-12-26 15:39:19 +0000

    add support for docx file types in preview + make x button a flex instead of absolute

--- frontend/src/components/FileViewer.jsx
+++ frontend/src/components/FileViewer.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { TbX } from "react-icons/tb";
+import { renderAsync } from "docx-preview";
 
 const FilePreview = ({ fileBlob, onClose }) => {
   const [filePreview, setFilePreview] = useState(null);
   const [fileType, setFileType] = useState("");
+  const previewBodyContainerRef = useRef(null);
 
   useEffect(() => {
     if (fileBlob) {
@@ -21,6 +23,36 @@ const FilePreview = ({ fileBlob, onClose }) => {
         setFilePreview(fileURL);
       } else if (fileType === "application/pdf") {
         setFilePreview(fileURL);
+      } else if (fileType === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+        const fileReader = new FileReader();
+        fileReader.onload = async (event) => {
+          const arrayBuffer = event.target.result;
+          if (previewBodyContainerRef.current) {
+            // https://github.com/VolodymyrBaydalka/docxjs?tab=readme-ov-file#readme
+            const options = {
+              className: "docx", // class name/prefix for default and document style classes
+              inWrapper: true, // enables rendering of wrapper around document content
+              ignoreWidth: false, // disables rendering width of page
+              ignoreHeight: false, // disables rendering height of page
+              ignoreFonts: false, // disables fonts rendering
+              breakPages: true, // enables page breaking on page breaks
+              ignoreLastRenderedPageBreak: false, // disables page breaking on lastRenderedPageBreak elements
+              experimental: false, // enables experimental features (tab stops calculation)
+              trimXmlDeclaration: true, // if true, xml declaration will be removed from xml documents before parsing
+              useBase64URL: false, // if true, images, fonts, etc. will be converted to base 64 URL, otherwise URL.createObjectURL is used
+              renderChanges: false, // enables experimental rendering of document changes (insertions/deletions)
+              renderHeaders: true, // enables headers rendering
+              renderFooters: true, // enables footers rendering
+              renderFootnotes: true, // enables footnotes rendering
+              renderEndnotes: true, // enables endnotes rendering
+              renderComments: false, // enables experimental comments rendering
+              debug: false, // enables additional logging
+            };
+
+            await renderAsync(new Uint8Array(arrayBuffer), previewBodyContainerRef.current, null, options);
+          }
+        };
+        fileReader.readAsArrayBuffer(fileBlob);
       } else {
         setFilePreview(null);
       }
@@ -36,9 +68,11 @@ const FilePreview = ({ fileBlob, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-95 flex items-center justify-center z-50">
       <div className="relative w-full h-full p-6 flex flex-col">
-        <button onClick={onClose} className="absolute top-4 right-4 text-gray-400 hover:text-red-500">
-          <TbX size={24} />
-        </button>
+        <div className="flex justify-end">
+          <button onClick={onClose} className="text-gray-400 hover:text-red-500">
+            <TbX size={24} />
+          </button>
+        </div>
 
         <div className="flex-grow flex items-center justify-center rounded-md overflow-hidden">
           {/* image/ */}
@@ -59,6 +93,19 @@ const FilePreview = ({ fileBlob, onClose }) => {
           {/* application/pdf */}
           {fileType === "application/pdf" && (
             <embed src={filePreview} type="application/pdf" className="w-full h-full" />
+          )}
+
+          {/* application/vnd.openxmlformats-officedocument.wordprocessingml.document */}
+          {fileType === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" && (
+            <>
+              <p className="text-copy-primary/80 md:hidden">This file cannot be previewed on mobile.</p>
+
+              <div className="h-full w-full hidden md:block">
+                <span className="text-copy-secondary">The styling of the file may not display correctly.</span>
+
+                <div ref={previewBodyContainerRef} className="w-full h-full overflow-auto text-left" />
+              </div>
+            </>
           )}
 
           {/* not matched */}
