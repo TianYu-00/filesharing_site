45ff49c0a1dcf02639de1c0b356b7dced24c21ba
TianYu-00 <imniyeye@gmail.com>
2024-12-30 15:01:36 +0000

    refactor file Delete Confirmation Modal into component

--- frontend/src/pages/Landing_MyFiles.jsx
+++ frontend/src/pages/Landing_MyFiles.jsx
@@ -42,6 +42,7 @@ import {
   TbX,
   TbEye,
 } from "react-icons/tb";
+import DeleteConfirmationModal from "../components/MyFiles/DeleteConfirmationModal";
 
 function Landing_MyFiles() {
   const [isLoadingPage, setIsLoadingPage] = useState(true);
@@ -698,38 +699,15 @@ function Landing_MyFiles() {
           />
         )}
 
-        {/* Delete Confirmation Modal */}
+        {/* New Delete Confirmation Modal */}
         {isDeleteConfirmationModalOpen && (
-          <Modal
-            isOpen={isDeleteConfirmationModalOpen}
-            onClose={() => {
-              setIsDeleteConfirmModalOpen(false);
-              setCurrentSelectedFile(null);
-            }}
-            modalTitle={`Delete Confirmation`}
-          >
-            <p className="text-copy-primary text-lg mb-4">Are you sure you want to delete the file?</p>
-            <p className="text-copy-secondary text-lg mb-4">{currentSelectedFile.originalname}</p>
-            <div className="flex justify-center items-align space-x-6">
-              <button
-                className="bg-cta font-bold p-2 rounded text-cta-text hover:bg-cta-active transition duration-500 ease-in-out"
-                onClick={() => {
-                  setIsDeleteConfirmModalOpen(false);
-                  setCurrentSelectedFile(null);
-                }}
-              >
-                Cancel
-              </button>
-              <button
-                className="bg-red-500 font-bold p-2 rounded text-white hover:bg-red-700 transition duration-500 ease-in-out"
-                onClick={() => {
-                  handle_FileDelete(currentSelectedFile.id);
-                }}
-              >
-                Confirm
-              </button>
-            </div>
-          </Modal>
+          <DeleteConfirmationModal
+            isDeleteConfirmationModalOpen={isDeleteConfirmationModalOpen}
+            setIsDeleteConfirmModalOpen={setIsDeleteConfirmModalOpen}
+            setCurrentSelectedFile={setCurrentSelectedFile}
+            currentSelectedFile={currentSelectedFile}
+            handle_FileDelete={handle_FileDelete}
+          />
         )}
 
         {/* Rename Modal */}
