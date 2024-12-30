e9e110380160e82c1a7432270b0df82fe0e93e2d
TianYu-00 <imniyeye@gmail.com>
2024-12-30 15:51:52 +0000

    refactor file side panel into component

---
+++ frontend/src/components/MyFiles/SidePanel.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useUser } from "../../context/UserContext";
+
+import { animated } from "@react-spring/web";
+
+function SidePanel({
+  setIsSideBarOpen,
+  sideBarDrawerAnimation,
+  TbX,
+  pageState,
+  handle_AllFiles,
+  TbFiles,
+  handle_AllFavourite,
+  TbStar,
+  handle_AllTrash,
+  TbTrash,
+}) {
+  const { user } = useUser();
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-screen z-50">
+      <div
+        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-80`}
+        onClick={() => setIsSideBarOpen(false)}
+      />
+      <animated.div
+        className={`flex flex-col text-white bg-background w-full h-full p-4 max-w-sm`}
+        style={sideBarDrawerAnimation}
+      >
+        <div className="p-2 py-4 relative">
+          <button
+            className="text-copy-secondary text-xl font-bold px-2 absolute right-0 top-4 hover:text-red-700"
+            onClick={() => setIsSideBarOpen(false)}
+          >
+            <TbX size={17} strokeWidth={3} />
+          </button>
+          <p className="text-3xl font-bold text-copy-primary whitespace-nowrap overflow-hidden truncate">
+            {user?.username}
+          </p>
+          <p className="text-copy-secondary whitespace-nowrap overflow-hidden truncate">{user?.email}</p>
+        </div>
+
+        <div className="border border-border mb-4"></div>
+
+        <div className="text-copy-primary">
+          <div className="my-2">
+            <button
+              className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
+                pageState === "all" ? "text-copy-opp bg-background-opp" : ""
+              }`}
+              onClick={handle_AllFiles}
+            >
+              <TbFiles />
+              <span className="pl-4">All Files</span>
+            </button>
+          </div>
+          <div className="my-2">
+            <button
+              className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
+                pageState === "favourite" ? "text-copy-opp bg-background-opp" : ""
+              }`}
+              onClick={handle_AllFavourite}
+            >
+              <TbStar />
+              <span className="pl-4">Favourite</span>
+            </button>
+          </div>
+          <div className="my-2">
+            <button
+              className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
+                pageState === "trash" ? "text-copy-opp bg-background-opp" : ""
+              }`}
+              onClick={handle_AllTrash}
+            >
+              <TbTrash />
+              <span className="pl-4">Trash</span>
+            </button>
+          </div>
+        </div>
+      </animated.div>
+    </div>
+  );
+}
+
+export default SidePanel;
--- frontend/src/pages/Landing_MyFiles.jsx
+++ frontend/src/pages/Landing_MyFiles.jsx
@@ -46,6 +46,7 @@ import DeleteConfirmationModal from "../components/MyFiles/DeleteConfirmationMod
 import RenameModal from "../components/MyFiles/RenameModal";
 import ManageLinkModal from "../components/MyFiles/ManageLinkModal";
 import FileActionPanel from "../components/MyFiles/FileActionPanel";
+import SidePanel from "../components/MyFiles/SidePanel";
 
 function Landing_MyFiles() {
   const [isLoadingPage, setIsLoadingPage] = useState(true);
@@ -769,69 +770,19 @@ function Landing_MyFiles() {
         />
 
         <div className="w-full">
-          {/* Side Bar ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/}
           {isSideBarOpen && (
-            <div className="fixed top-0 left-0 w-full h-screen z-50">
-              <div
-                className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-80`}
-                onClick={() => setIsSideBarOpen(false)}
-              />
-              <animated.div
-                className={`flex flex-col text-white bg-background w-full h-full p-4 max-w-sm`}
-                style={sideBarDrawerAnimation}
-              >
-                <div className="p-2 py-4 relative">
-                  <button
-                    className="text-copy-secondary text-xl font-bold px-2 absolute right-0 top-4 hover:text-red-700"
-                    onClick={() => setIsSideBarOpen(false)}
-                  >
-                    <TbX size={17} strokeWidth={3} />
-                  </button>
-                  <p className="text-3xl font-bold text-copy-primary whitespace-nowrap overflow-hidden truncate">
-                    {user?.username}
-                  </p>
-                  <p className="text-copy-secondary whitespace-nowrap overflow-hidden truncate">{user?.email}</p>
-                </div>
-
-                <div className="border border-border mb-4"></div>
-
-                <div className="text-copy-primary">
-                  <div className="my-2">
-                    <button
-                      className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
-                        pageState === "all" ? "text-copy-opp bg-background-opp" : ""
-                      }`}
-                      onClick={handle_AllFiles}
-                    >
-                      <TbFiles />
-                      <span className="pl-4">All Files</span>
-                    </button>
-                  </div>
-                  <div className="my-2">
-                    <button
-                      className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
-                        pageState === "favourite" ? "text-copy-opp bg-background-opp" : ""
-                      }`}
-                      onClick={handle_AllFavourite}
-                    >
-                      <TbStar />
-                      <span className="pl-4">Favourite</span>
-                    </button>
-                  </div>
-                  <div className="my-2">
-                    <button
-                      className={`w-full text-left rounded-lg p-2 py-3 flex items-center hover:text-copy-opp hover:bg-background-opp ${
-                        pageState === "trash" ? "text-copy-opp bg-background-opp" : ""
-                      }`}
-                      onClick={handle_AllTrash}
-                    >
-                      <TbTrash />
-                      <span className="pl-4">Trash</span>
-                    </button>
-                  </div>
-                </div>
-              </animated.div>
-            </div>
+            <SidePanel
+              setIsSideBarOpen={setIsSideBarOpen}
+              sideBarDrawerAnimation={sideBarDrawerAnimation}
+              TbX={TbX}
+              pageState={pageState}
+              handle_AllFiles={handle_AllFiles}
+              TbFiles={TbFiles}
+              handle_AllFavourite={handle_AllFavourite}
+              TbStar={TbStar}
+              handle_AllTrash={handle_AllTrash}
+              TbTrash={TbTrash}
+            />
           )}
 
           {/* Table ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/}
