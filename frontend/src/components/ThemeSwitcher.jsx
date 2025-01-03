71b805b7ecb78bf4956ad333cf8be8d254d474c5
TianYu-00 <imniyeye@gmail.com>
2025-01-03 03:17:10 +0000

    update: theme switcher accessibility

--- frontend/src/components/ThemeSwitcher.jsx
+++ frontend/src/components/ThemeSwitcher.jsx
@@ -3,7 +3,7 @@ import { TbSun, TbMoon } from "react-icons/tb";
 
 function ThemeSwitcher({ isToggled, toggleSwitch }) {
   return (
-    <button onClick={toggleSwitch} className="p-2">
+    <button onClick={toggleSwitch} className="p-2" aria-label="Toggle Theme">
       {isToggled ? <TbSun className="text-black" size={25} /> : <TbMoon className="text-white" size={25} />}
     </button>
   );
