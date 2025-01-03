87e9146eff753b234796d90887b14f6e701775c6
TianYu-00 <imniyeye@gmail.com>
2025-01-03 23:39:35 +0000

    update: add tests for user token verification endpoint

--- backend/__tests__/endpoints_auth.test.js
+++ backend/__tests__/endpoints_auth.test.js
@@ -369,3 +369,25 @@ describe("PATCH api/auth/reset-password", () => {
     }
   });
 });
+
+/////////////////////////////////////////////////////////////////////////// VERIFY USER TOKEN
+describe("GET api/auth/verify-user-token", () => {
+  test("should return a 401 status code, indicating the user is not logged in", async () => {
+    try {
+      await request(app).get("/api/auth/verify-user-token").expect(401);
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  test("should return a 200 status code, indicating the user is logged in", async () => {
+    try {
+      const tempUserLoginCredentials = data.users[0];
+      const loginResponse = await request(app).post("/api/auth/login").send(tempUserLoginCredentials).expect(200);
+      const cookies = loginResponse.headers["set-cookie"];
+      await request(app).get("/api/auth/verify-user-token").set("Cookie", cookies).expect(200);
+    } catch (error) {
+      throw error;
+    }
+  });
+});
