7f4f09d670ac37b961f81b64e44ba899bad7a404
TianYu-00 <imniyeye@gmail.com>
2025-01-04 17:15:48 +0000

    update: add GET /api/files test

--- backend/__tests__/files/endpoints_files.test.js
+++ backend/__tests__/files/endpoints_files.test.js
@@ -7,20 +7,19 @@ beforeEach(async () => {
   await seed(data);
 });
 
-/////////////////////////////////////////////////////////////////////////// USERS
-describe("GET /api/users", () => {
+/////////////////////////////////////////////////////////////////////////// FILES
+describe("GET /api/files", () => {
   test("should return a 403 status code, indicating users cannot access the list of all users", async () => {
     const tempUserLoginCredentials = data.users[0];
     const loginResponse = await request(app).post("/api/auth/login").send(tempUserLoginCredentials).expect(200);
     const cookies = loginResponse.headers["set-cookie"];
-    await request(app).get("/api/users").set("Cookie", cookies).expect(403);
+    await request(app).get("/api/files").set("Cookie", cookies).expect(403);
   });
 
-  test("should return a 200 status code, indicating an admin successfully retrieves the list of all users", async () => {
+  test("should return a 200 status code, indicating an admin successfully retrieves the list of all files", async () => {
     const tempUserLoginCredentials = data.users[1];
     const loginResponse = await request(app).post("/api/auth/login").send(tempUserLoginCredentials).expect(200);
     const cookies = loginResponse.headers["set-cookie"];
-    const { body } = await request(app).get("/api/users").set("Cookie", cookies).expect(200);
-    expect(body.data).toHaveLength(2);
+    await request(app).get("/api/files").set("Cookie", cookies).expect(200);
   });
 });
