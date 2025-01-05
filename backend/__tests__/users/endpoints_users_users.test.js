3304aa753fe6a8925072e4e15fb2bd4f0d1c61e9
TianYu-00 <imniyeye@gmail.com>
2025-01-05 10:02:06 +0000

    fix: update test to reflect the new seed data

--- backend/__tests__/users/endpoints_users_users.test.js
+++ backend/__tests__/users/endpoints_users_users.test.js
@@ -21,6 +21,6 @@ describe("GET /api/users", () => {
     const loginResponse = await request(app).post("/api/auth/login").send(tempUserLoginCredentials).expect(200);
     const cookies = loginResponse.headers["set-cookie"];
     const { body } = await request(app).get("/api/users").set("Cookie", cookies).expect(200);
-    expect(body.data).toHaveLength(2);
+    expect(body.data).toHaveLength(3);
   });
 });
