ba55c14543a93c5c64be0b3c0260cc1625f3c2ff
TianYu-00 <imniyeye@gmail.com>
2025-01-18 08:06:42 +0000

    update: change app.js to take env frontend url + add vercel.json

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const apiRouter = require("./routes/api-router");
 
 const app = express();
 const corsConfigOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: process.env.FRONTEND_URL || "http://127.0.0.1:5173",
   credentials: true,
 };
 // Middlewares
