7b304d5a10318e751dc33a947e564031b79e6c18
TianYu-00 <imniyeye@gmail.com>
2025-02-05 00:22:59 +0000

    feat: add informational note to PageLoader component.

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -4,6 +4,16 @@ import HashLoader from "react-spinners/HashLoader";
 function PageLoader({ isLoading, timer = 2000, message = "", children }) {
   const [shouldDisplayLoader, setShouldDisplayLoader] = useState(false);
 
+  const [note, setNote] = useState("");
+
+  useEffect(() => {
+    const tempNote =
+      process.env.NODE_ENV === "production"
+        ? "Please note the server may take 50+ seconds to spin back up and respond after periods of inactivity"
+        : "";
+    setNote(`${tempNote}`);
+  }, []);
+
   useEffect(() => {
     if (isLoading) {
       setShouldDisplayLoader(true);
@@ -22,7 +32,8 @@ function PageLoader({ isLoading, timer = 2000, message = "", children }) {
     return (
       <div className="fixed top-0 left-0 h-screen w-screen z-50 flex items-center justify-center bg-background overflow-hidden flex flex-col">
         <HashLoader color="#1764FF" />
-        <p className="text-copy-secondary pt-4 animate-pulse">{message}</p>
+        <p className="text-copy-secondary pt-4 animate-pulse text-center px-4">{message}</p>
+        <p className="text-copy-secondary pt-10 text-center px-4">{note}</p>
       </div>
     );
   } else {
